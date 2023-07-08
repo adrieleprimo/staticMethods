a2412056710682385e7b3d3ecde6b1040dbbe693
Adriele <83988106+adrieleprimo@users.noreply.github.com>
2023-07-08 14:55:22 -0300

    Update programDollar.java

--- programDollar.java
+++ programDollar.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 import util.Calculator;
 
-public class Program {
+public class programDollar {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
